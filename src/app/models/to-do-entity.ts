f212be6cc05b1bb2c21174096b0f3fb67bae0407
pegpapas <peg.papas@gmail.com>
2019-06-30 20:34:27 +0300

    to-do-list page

---
+++ src/app/models/to-do-entity.ts
@@ -0,0 +1,7 @@
+export class ToDoEntity{
+    id: number;
+    isDone: boolean;
+    title: string;
+    description: string;
+    endDate: string;
+}
--- src/app/modules/feature/to-do-list/to-do-list.component.ts
+++ src/app/modules/feature/to-do-list/to-do-list.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoEntity } from 'src/app/models/to-do-entity';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html'
 })
 export class ToDoListComponent implements OnInit {
+  header: any;
+  toDoList: ToDoEntity[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.header = {
+      description: 'Description',
+      title: 'Title',
+      completed: 'Completed',
+      date: 'Date',
+      actions: 'Actions'
+    };
+    this.toDoList.push({ id: 1, description: 'test', title: '3', isDone: true, endDate: '15/5/2019' });
+    this.toDoList.push({ id: 1, description: 'test', title: '3', isDone: true, endDate: '15/5/2019' });
   }
 
 }
